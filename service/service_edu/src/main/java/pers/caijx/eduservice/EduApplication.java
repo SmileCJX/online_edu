afe022aadb6df932b97e7f254612596643bc1283
SmileCJX <1092925421@qq.com>
2020-05-24 21:57:04 +0800

    swagger整合

--- service/service_edu/src/main/java/pers/caijx/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/pers/caijx/eduservice/EduApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @Version V1.0
  **/
 @SpringBootApplication
+@ComponentScan(basePackages = {"pers.caijx"})
 public class EduApplication {
 
     public static void main(String[] args) {
--- service/service_edu/src/main/java/pers/caijx/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/pers/caijx/eduservice/controller/EduTeacherController.java
@@ -1,6 +1,9 @@
 package pers.caijx.eduservice.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +20,7 @@ import java.util.List;
  * @author JunXiangCai
  * @since 2020-05-24
  */
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
@@ -32,6 +36,7 @@ public class EduTeacherController {
      * 访问地址为http://localhost:8001/eduservice/teacher/findAll
      * @return
      */
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("/findAll")
     public List<EduTeacher> findAllTeacher() {
         // 调用service的方式完成查询所有的操作
@@ -43,8 +48,9 @@ public class EduTeacherController {
      * 逻辑删除讲师的方法
      * @return
      */
-    @DeleteMapping({"id"})
-    public boolean removeTeacher(@PathVariable String id) {
+    @ApiOperation(value = "根据ID删除讲师")
+    @DeleteMapping({"{id}"})
+    public boolean removeTeacher(@ApiParam(name = "id",value = "讲师ID",required = true)  @PathVariable String id) {
         boolean flag = eduTeacherService.removeById(id);
         return flag;
     }
