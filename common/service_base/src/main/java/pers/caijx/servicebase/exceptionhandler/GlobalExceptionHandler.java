3d79682b0a035918262f253e4295890071a96a44
SmileCJX <1092925421@qq.com>
2020-05-26 00:08:11 +0800

    全局异常处理以及依赖传递

---
+++ common/service_base/src/main/java/pers/caijx/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package pers.caijx.servicebase.exceptionhandler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pers.caijx.commonutils.R;
+
+/**
+ * @ClassName GlobalExceptionHandler
+ * @Description: TODO
+ * @Author Think
+ * @Date 2020/5/25
+ * @Version V1.0
+ **/
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 指定出现了什么异常执行这个方法
+    @ExceptionHandler(Exception.class)
+    @ResponseBody // 为了返回数据
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.error().message("执行了全局异常处理....");
+    }
+}
